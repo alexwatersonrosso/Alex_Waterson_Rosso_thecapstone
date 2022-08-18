2822dbe2734453fbe08bc91c0b58fa2215abaf44
Alex Waterson-Rosso <89030416+alexwatersonrosso@users.noreply.github.com>
2022-08-17 23:16:20 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        DB_LOCAL_URI: 'mongodb://localhost:27017',
+        DB_URI: '',
+
+        STRIPE_API_KEY: 'pk_test_51LOrwnKNkWwS7FOTQlW1poSEN0l7OrrtnXkNRc3NgU0wuH9ORssmUBumBA8PKLzpeEzO15hVGUkgV0nBPFEK8qsS00gtFPXznK',
+        STRIPE_SECRET_KEY: '',
+
+        STRIPE_WEBHOOK_SECRET: '',
+
+        CLOUDINARY_CLOUD_NAME: '',
+        CLOUDINARY_API_KEY: '',
+        CLOUDINARY_API_SECRET: '',
+
+        SMTP_HOST: "",
+        SMTP_PORT: "",
+        SMTP_USER: "",
+        SMTP_PASSWORD: "",
+        SMTP_FROM_EMAIL: "",
+        SMTP_FROM_NAME: "",
+
+        NEXTAUTH_URL: '',
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
